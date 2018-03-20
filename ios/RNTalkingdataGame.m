d54d0877b17964eee9c52a5fb61ab6b62dda2f68
MaTHtoE <canyara@gmail.com>
2018-03-20 15:06:24 +0800

    实现ios所需接口

--- ios/RNTalkingdataGame.m
+++ ios/RNTalkingdataGame.m
@@ -1,5 +1,6 @@
 
 #import "RNTalkingdataGame.h"
+#import "TalkingDataGA.h"
 
 @implementation RNTalkingdataGame
 
@@ -8,6 +9,48 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(onStart:(NSString *)appId withChannelId:(NSString *)channelId withLog:(BOOL)bLog)
+{
+    [TalkingDataGA setVerboseLogDisabled:!bLog];
+    [TalkingDataGA onStart:appId withChannelId:channelId];
+}
+
+RCT_EXPORT_METHOD(setAccountName:(NSString *)accountId (NSString *)accountName)
+{
+    NSString* aid = accountId;
+    
+    if (aid == nil) {
+        aid = [TalkingDataGA getDeviceId];
+    }
+    
+    TDGAAccount* account = [TDGAAccount setAccount:aid];
+    [account setAccountName:accountName];
+}
+
+RCT_EXPORT_METHOD(setAccountType:(NSString *)accountId (int)accountType)
+{
+    NSString* aid = accountId;
+    
+    if (aid == nil) {
+        aid = [TalkingDataGA getDeviceId];
+    }
+    
+    TDGAAccount* account = [TDGAAccount setAccount:aid];
+    [account setAccountType:(TDGAAccountType)accountType];
+}
+
+RCT_EXPORT_METHOD(setAccountType:(NSString *)accountId (int)accountType)
+{
+    NSString* aid = accountId;
+    
+    if (aid == nil) {
+        aid = [TalkingDataGA getDeviceId];
+    }
+    
+    TDGAAccount* account = [TDGAAccount setAccount:aid];
+    [account setAccountType:(TDGAAccountType)accountType];
+}
 
 @end
   
