b1c55597c5ffb56871402a6458941a033e38bc5f
MaTHtoE <canyara@gmail.com>
2018-12-28 16:32:50 +0800

    加入android实现

--- android/src/main/java/com/talkingdata/RNTalkingdataGameModule.java
+++ android/src/main/java/com/talkingdata/RNTalkingdataGameModule.java
@@ -1,10 +1,18 @@
 
 package com.talkingdata;
 
+import android.app.Application;
+import android.content.Context;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.tendcloud.tenddata.TDGAAccount;
+import com.tendcloud.tenddata.TalkingDataGA;
+
+import java.util.Map;
 
 public class RNTalkingdataGameModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +26,40 @@ public class RNTalkingdataGameModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNTalkingdataGame";
+  }
+
+  public static void start(Context app, String appID, String channel) {
+    // App ID: 在TalkingData Game Analytics创建应用后会得到App ID。
+    // 渠道 ID: 是渠道标识符，可通过不同渠道单独追踪数据。
+    TalkingDataGA.init(app, appID, channel);
+  }
+
+  @ReactMethod
+  public void onEvent(String eventId, final ReadableMap eventData) {
+    TalkingDataGA.onEvent(eventId, eventData.toHashMap());
+  }
+
+  @ReactMethod
+  public void setAccountName(String accountId, String accountName) {
+    TDGAAccount account = TDGAAccount.setAccount(accountId);
+    if (account != null) {
+      account.setAccountName(accountName);
+    }
+  }
+
+  @ReactMethod
+  public void setAcountLevel(String accountId, int level) {
+    TDGAAccount account = TDGAAccount.setAccount(accountId);
+    if (account != null) {
+      account.setLevel(level);
+    }
+  }
+
+  @ReactMethod
+  public void setAccountType(String accountId, int accountType) {
+    TDGAAccount account = TDGAAccount.setAccount(accountId);
+    if (account != null) {
+      account.setAccountType(TDGAAccount.AccountType.values()[accountType]);
+    }
   }
 }
